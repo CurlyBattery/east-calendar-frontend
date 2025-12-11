c9952f75948c9d8e3a9c3ab0e124bfd3ac625846
ArtemKosyrev <artemkosyrev93@gmail.com>
2025-12-11 15:45:14 +0500

    feat: (scan)

--- src/pages/ScanPage/ScanPage.tsx
+++ src/pages/ScanPage/ScanPage.tsx
@@ -1,6 +1,9 @@
 import {useParams} from "react-router-dom";
+import { MdQrCodeScanner } from "react-icons/md";
+
 import {qrConfirm} from "../../http/auth.api.ts";
 import {QrStatus} from "../../types/user.ts";
+import './_scan.scss';
 
 type ScanPageParams = {
     sessionId: string;
@@ -19,11 +22,41 @@ const ScanPage = () => {
     };
 
     return (
-        <div>
-            Вы хотите войти в аккаунт на новом устройстве?
-            <button onClick={handleYes} type='button'>Да, войти</button>
-            <button onClick={handleNo} type='button'>Нет, отменить</button>
-            {sessionId}
+        <div className='scan'>
+            <div className='scan__container'>
+                <div className='scan__container__icon'>
+                    <MdQrCodeScanner/>
+                </div>
+
+                <div className='scan__container__title'>Подверждение входа</div>
+
+                <p className='scan__container__description'>
+                    Вы пытаетесь войти в свой аккаунт EastCalendar на новом устройстве.
+                    Подтвердите, что это действительно вы.
+                </p>
+
+                <div className='scan__container__actions'>
+                    <button
+                        onClick={handleYes}
+                        type='button'
+                        className='scan__container__actions__button scan__container__actions__button__confirm'
+                    >
+                        ✓ Да, это я
+                    </button>
+                    <button
+                        onClick={handleNo}
+                        type='button'
+                        className='scan__container__actions__button scan__container__actions__button__cancel'
+                    >
+                        ✕ Нет, отменить
+                    </button>
+                </div>
+
+                <div className='scan__container__info'>
+                    Если это были не вы немедленно нажмите "Отменить"
+                    и сменить пароль.
+                </div>
+            </div>
         </div>
     );
 };
